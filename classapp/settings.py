b17faaafd9c4f06db7ced4beef6ec24c7889595f
Lee <leeneufeld23@hotmail.com>
2019-07-03 11:55:26 -0600

    settings.py

--- classapp/settings.py
+++ classapp/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = https://storage.googleapis.com/class-app/static/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'classapp/static')
 ]
